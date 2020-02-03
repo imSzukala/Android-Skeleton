15fee9d14641e2260aec423998534d308d7b8a65
Irena Szukala <i.szukala@dook.pro>
2020-02-03 11:41:10 +0100

    Update dependencies versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -22,35 +22,35 @@ object Versions {
     const val coroutines = "1.3.2"
 
     // AndroidX
-    const val core = "1.2.0-rc01"
-    const val appCompat = "1.1.0"
-    const val constraintLayout = "1.1.3"
+    const val core = "1.3.0-alpha01"
+    const val appCompat = "1.2.0-alpha02"
+    const val constraintLayout = "2.0.0-beta4"
     const val recyclerView = "1.1.0-rc01"
     const val cardView = "1.0.0"
 
     // Room
-    const val room = "2.2.2"
+    const val room = "2.2.3"
 
     // Ui
-    const val material = "1.2.0-alpha02"
+    const val material = "1.2.0-alpha04"
     const val lottie = "3.1.0"
 
     // Navigation
-    const val navigation = "2.2.0-rc02"
+    const val navigation = "2.2.0"
 
     // Lifecycle
-    const val lifecycle = "2.2.0-rc02"
+    const val lifecycle = "2.2.0"
 
     // Di
-    const val koin = "2.0.1"
+    const val koin = "2.1.0-alpha-10"
 
     // Networking
     const val moshi = "1.9.2"
-    const val okHttp = "4.2.2"
-    const val retrofit = "2.6.2"
+    const val okHttp = "4.3.1"
+    const val retrofit = "2.7.1"
 
     // Debug tools
-    const val leakCanary = "2.0"
+    const val leakCanary = "2.1"
     const val stetho = "1.5.1"
 
     // Permissions
@@ -60,16 +60,16 @@ object Versions {
     const val threeTen = "1.2.1"
 
     // Unit Testing
-    const val jUnit5 = "5.6.0-M1"
+    const val jUnit5 = "5.6.0"
     const val coreTest = "2.1.0"
     const val mockK = "1.9.3"
 
     // Instrumentation Testing
-    const val espressoCore = "3.2.0"
-    const val testRunner = "1.2.0"
+    const val espressoCore = "3.3.0-alpha03"
+    const val testRunner = "1.3.0-alpha03"
     const val jUnit5Runner = "1.2.0"
 
     // Quality
-    const val ktlint = "0.35.0"
+    const val ktlint = "0.36.0"
     const val gradleVersions = "0.27.0"
 }
